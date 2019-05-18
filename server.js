695116443b464e8d1fc7d1bf171eb2abeb824cc2
hiep294 <quanghiep294@gmail.com>
2019-05-18 09:00:41 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const config = require('config')
+
+const app = express()
+
+// bodyparse middleware
+app.use(express.json())
+
+// db config
+const db = config.get('mongoURI')
+
+//connect to mongo
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log('mongodb connected'))
+  .catch(err => console.log(err));
+
+//Use routes
+app.use('/api/items', require('./routes/api/items'))
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Server starting at port ${port}`))
+
